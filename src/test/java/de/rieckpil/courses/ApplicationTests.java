6ab4bbd684a7799e8ec9c74b9be697547ec72c7e
rieckpil <mail@philipriecks.de>
2020-07-30 08:21:37 +0200

    next steps for JUnit

--- src/test/java/de/rieckpil/courses/ApplicationTests.java
+++ src/test/java/de/rieckpil/courses/ApplicationTests.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-@Disabled
+// @SpringBootTest
+// @Disabled
 class ApplicationTests {
 
-  @Test
+  // @Test
   void contextLoads() {
   }
 
--- src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
@@ -8,11 +8,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+<<<<<<< HEAD
 import static de.rieckpil.courses.book.review.RandomReviewParameterResolverExtension.RandomReview;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(RandomReviewParameterResolverExtension.class)
+=======
+import static de.rieckpil.courses.book.review.RandomReviewParameterResolver.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(RandomReviewParameterResolver.class)
+>>>>>>> next steps for JUnit
 class ReviewVerifierTest {
 
   private ReviewVerifier reviewVerifier;
@@ -23,7 +31,9 @@ class ReviewVerifierTest {
   }
 
   @Test
-  void shouldFailWhenReviewContainsSwearWord() {
+  void shouldFailWhenReviewContainsSwearWord() throws InterruptedException {
+
+    Thread.sleep(1000);
     String review = "This book is shit";
     System.out.println("Testing a review");
 
@@ -32,8 +42,15 @@ class ReviewVerifierTest {
   }
 
   @Test
+<<<<<<< HEAD
   @DisplayName("Should fail when review contains 'lorem ipsum'")
   void testLoremIpsum() {
+=======
+  @DisplayName("Should fail when review contains 'Lorem Ipsum'")
+  void nextTest() throws InterruptedException {
+
+    Thread.sleep(1000);
+>>>>>>> next steps for JUnit
 
     String review = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr," +
       " sed diam nonumy eirmod tempor invidunt ut labore et " +
@@ -44,9 +61,18 @@ class ReviewVerifierTest {
     assertFalse(result, "ReviewVerifier did not detect lorem ipsum");
   }
 
+<<<<<<< HEAD
   @ParameterizedTest
   @CsvFileSource(resources = "/badReview.csv")
   void shouldFailWhenReviewIsOfBadQuality(String review) {
+=======
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "/badReviews.csv")
+  void shouldFailWhenReviewIsOfBadQuality(String review) throws InterruptedException {
+    Thread.sleep(1000);
+
+>>>>>>> next steps for JUnit
     boolean result = reviewVerifier.doesMeetQualityStandards(review);
     assertFalse(result, "ReviewVerifier did not detect bad review");
   }
@@ -55,16 +81,32 @@ class ReviewVerifierTest {
   void shouldFailWhenRandomReviewQualityIsBad(@RandomReview String review) {
     System.out.println(review);
     boolean result = reviewVerifier.doesMeetQualityStandards(review);
+<<<<<<< HEAD
     assertFalse(result, "ReviewVerifier did not detect random bad review");
   }
 
   @Test
   void shouldPassWhenReviewIsGood() {
+=======
+    assertFalse(result, "ReviewVerifier did not detect bad review");
+  }
+
+  @Test
+  void shouldPassWhenReviewIsGood() throws InterruptedException {
+
+    Thread.sleep(1000);
+
+>>>>>>> next steps for JUnit
     String review = "I can totally recommend this book " +
       "who is interested in learning how to write Java code!";
 
     boolean result = reviewVerifier.doesMeetQualityStandards(review);
+<<<<<<< HEAD
     assertTrue(result, "ReviewVerifier did not pass a good review");
   }
 
+=======
+    assertTrue(result, "ReviewVerifier did not pass good review");
+  }
+>>>>>>> next steps for JUnit
 }
